9ec8c292c898655a75b9a33ebbe83212fc392e10
SanMarAbr <64150546+SanMarAbr@users.noreply.github.com>
2023-08-24 02:23:25 -0600

    first commit

---
+++ Capitulo_2/Ejercicios/actividades_Cap2.r
@@ -0,0 +1,7 @@
+# 1. Genera los objetos necesarios para que Z sea el resultado del área de un círculo
+r <- 5
+z <- pi * (r^2)
+# 2. Cuál es el valor del resultado del área del circulo
+z  # [1] 78.539827
+# 3. Genera una secuencia de números que tomen valores de 0.5 en 0.5 comenzando en 1 y terminando en 100
+seq(1,100, by = 0.5)
---
+++ Capitulo_2/manejo_Objetos_R.r
@@ -0,0 +1,30 @@
+#operaciones_Simples
+a <- 3
+2+2
+pi*2
+(4+3)/(3+0.7)
+# declaración_Objetos
+x <- 3*4
+y <- 3*pi
+z <- x+y
+objects() # Visualización_De_Objetos
+remove(y,z)
+objects()
+
+base_triangulo <- 10 
+altura_triangulo <- 30
+area_triangulo <- (base_triangulo*altura_triangulo)/2
+
+area_triangulo
+
+# Funciones en R 
+nombre <- pi
+nombre
+
+seq(1,100, by = 1) +
+16
+
+hi <- "Hello" # Se deben de terminar las comillas, etc para evitar que se haga una continuacion con la función
+
+?setwd
+getwd()
